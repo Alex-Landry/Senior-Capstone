94cbb9cefadb819da3dce4c7a87f6f5ea2b4fe1f
samuelmmorse <samuel.morse@maine.edu>
2022-01-29 16:08:04 -0500

    deleted src directory

--- src/boardmanlab/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class user(models.Model):
-    user_name = models.CharField(max_length=200)
-    user_email = models.EmailField(max_length=50)
-
