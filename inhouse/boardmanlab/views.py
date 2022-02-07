996ca1104ae78e99ec8783efd16487172c692117
samuelmmorse <samuel.morse@maine.edu>
2022-02-07 04:58:15 -0500

    added allauth library
    Implemented the oauth for google accounts, but the form is broken...

--- inhouse/boardmanlab/views.py
+++ inhouse/boardmanlab/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from oauth2_provider.views.generic import ProtectedResourceView
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-
+@login_required()
 def index(request):
     return render(request, 'index.html')
 
@@ -20,6 +22,14 @@ def helpsessions(request):
 def error(request):
     return render(request, 'error.html')
 
+
+#AUTHORIZED ONLY VIEWS
 class ApiEndpoint(ProtectedResourceView):
     def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello, OAuth2!')
+        return HttpResponse(
+            'Hello there! You are acting on behalf of "%s"\n'
+            % (request.user))
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
--- inhouse/inhouse/urls.py
+++ inhouse/inhouse/urls.py
@@ -1,25 +1,9 @@
-"""inhouse URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import include, path
 from boardmanlab.views import index, login, helpsessions, calendar, error
 import oauth2_provider.views as oauth2_views
 from django.conf import settings
-from boardmanlab.views import ApiEndpoint
+from boardmanlab.views import ApiEndpoint, Home
 
 # OAuth2 provider endpoints
 oauth2_endpoint_views = [
@@ -53,9 +37,11 @@ urlpatterns = [
     path('calendar/', calendar),
     path('error/', error),
     path('', login),
+    path('allauth', Home.as_view(), name='home'), # new
     # OAuth 2 endpoints:
     # need to pass in a tuple of the endpoints as well as the app's name
     # because the app_name attribute is not set in the included module
     path('o/', include((oauth2_endpoint_views, 'oauth2_provider'), namespace="oauth2_provider")),
     path('api/hello', ApiEndpoint.as_view()),  # an example resource endpoint
+    path('accounts/', include('allauth.urls')),
 ]
