5dabed6eb74b458f1fabf832cfb6c883545db71f
ForrestSwift <forrest.swift@maine.edu>
2022-02-15 14:47:36 -0500

    First User Extension

--- inhouse/users/models.py
+++ inhouse/users/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+    is_student = models.BooleanField(default=True)
+    is_helper = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
