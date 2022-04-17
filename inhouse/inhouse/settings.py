4d5325b22759380e618d6c93f354a8e102989e41
samuelmmorse <samuel.morse@maine.edu>
2022-04-17 02:41:46 -0400

    Update settings.py

--- inhouse/inhouse/settings.py
+++ inhouse/inhouse/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'NAME': env("DATABASE_NAME"),
         'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST", "localhost"),
+        'HOST': env("DATABASE_HOST"),
         'PORT': env("DATABASE_PORT"),
     }
 }
