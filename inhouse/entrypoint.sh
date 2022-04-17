ac084faf7251bfa467e58df145fec7fd477366a2
samuelmmorse <samuel.morse@maine.edu>
2022-04-17 12:39:14 -0400

    Update entrypoint.sh

--- inhouse/entrypoint.sh
+++ inhouse/entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-if [ "$1" = 'postgres' ]; then
-  # Run the postgresql entrypoint
-  . /docker-entrypoint.sh
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+    while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+      sleep 0.1
+    done
+    echo "PostgreSQL started"
 fi
+
+# It's okay to run the following two, flush and migrate commands on development mode(when debug mode is on) but not recommended
+# for production:
+
+# python manage.py flush --no-input
+# python manage.py migrate
+
+exec "$@"
