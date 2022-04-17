cd094c70991e556ddaa810dd52188fdb2be7169c
samuelmmorse <samuel.morse@maine.edu>
2022-04-17 10:42:36 -0400

    Update settings.py

--- inhouse/inhouse/settings.py
+++ inhouse/inhouse/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default=0)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost" "127.0.0.1" "[::1]"]
 
 SITE_ID = 1
 
