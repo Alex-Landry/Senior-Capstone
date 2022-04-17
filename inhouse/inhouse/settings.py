df591b8ffa552997787db743f6aaec65c3a32cef
samuelmmorse <samuel.morse@maine.edu>
2022-04-17 17:36:24 -0400

    Update settings.py

--- inhouse/inhouse/settings.py
+++ inhouse/inhouse/settings.py
@@ -154,6 +154,7 @@ USE_TZ = True
 
 # needs addressing at deployment
 STATIC_URL = "static/"
+STATIC_ROOT = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
