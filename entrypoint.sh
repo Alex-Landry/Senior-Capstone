ee37fb5f117e9730a6677c69a2dc5f66ed87cb12
samuelmmorse <samuel.morse@maine.edu>
2022-04-17 03:05:51 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+    while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+      sleep 0.1
+    done
+    echo "PostgreSQL started"
+fi
 
-gunicorn inhouse.wsgi:application --bind 0.0.0.0:8000
+# It's okay to run the following two, flush and migrate commands on development mode(when debug mode is on) but not recommended
+# for production:
+
+# python manage.py flush --no-input
+# python manage.py migrate
+
+exec "$@"
