0e34050f21e9a4ae780e1c1a13f9342e47d1c4d4
samuelmmorse <samuel.morse@maine.edu>
2022-04-17 10:52:49 -0400

    Update entrypoint.sh

--- inhouse/entrypoint.sh
+++ inhouse/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-    while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
-      sleep 0.1
-    done
-    echo "PostgreSQL started"
-fi
+
+echo "Waiting for postgres..."
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+  sleep 0.1
+done
+echo "PostgreSQL started"
 
 # It's okay to run the following two, flush and migrate commands on development mode(when debug mode is on) but not recommended
 # for production:
