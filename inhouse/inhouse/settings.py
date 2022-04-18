81732c780dffdd05a00eedaeca77df1a95b9cc09
samuelmmorse <samuel.morse@maine.edu>
2022-04-17 20:23:52 -0400

    rework

--- inhouse/inhouse/settings.py
+++ inhouse/inhouse/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "boardmanlab/static"), # Django will search for /static/
 ]
 STATIC_URL = "/static/"
-STATIC_ROOT = "/vol/web/static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
